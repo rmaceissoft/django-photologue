980293561a09c9887675fc89d1d9dd5f517540eb
Reiner Marquez <rmaceissoft@gmail.com>
2017-05-22 21:24:43 -0500

    fix broken migrations (sort_value can't be created twice)

--- photologue/migrations/0010_auto_20150924_1518.py
+++ photologue/migrations/0010_auto_20150924_1518.py
@@ -4,21 +4,16 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
-def add_sort_value_to_gallery_photos(apps, schema_editor):
+def forwards(apps, schema_editor):
+    """
     sql = schema_editor.sql_create_column % {
         'table':'photologue_gallery_photos',
         'column': 'sort_value',
         'definition': 'integer NOT NULL DEFAULT 0'
     }
     schema_editor.execute(sql)
-
-
-def delete_sort_value_to_gallery_photos(apps, schema_editor):
-    sql = schema_editor.sql_delete_column % {
-        'table': 'photologue_gallery_photos',
-        'column': 'sort_value'
-    }
-    schema_editor.execute(sql)
+    """
+    pass
 
 
 class Migration(migrations.Migration):
@@ -28,5 +23,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(add_sort_value_to_gallery_photos, reverse_code=delete_sort_value_to_gallery_photos)
+        migrations.RunPython(forwards)
     ]
